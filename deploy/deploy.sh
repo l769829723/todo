dededb12c14659f0d2476fdc1dbb26511589a75c
Evan <1054473703@qq.com>
2018-08-03 18:32:12 +0800

    update circleci config file

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-##### Delete ######
-SERVER_USERNAME='root'
-SERVER_PASSWORD='#H4q49KVCk{q(Mhz'
-SERVER_ADDRESS='45.77.125.156'
-
 # Pre-settings
 export ANSIBLE_CONFIG=deploy/ansible.cfg
 export ANSIBLE_HOST_KEY_CHECKING=False
@@ -13,7 +8,7 @@ export ANSIBLE_RECORD_HOST_KEYS=False
 # Render hosts file
 cat >/tmp/hosts<<EOF
 [servers]
-app_server ansible_host=${SERVER_ADDRESS} ansible_ssh_pass=${SERVER_PASSWORD} ansible_user=${SERVER_USERNAME}
+app_server ansible_host=${SERVER_ADDRESS} ansible_user=${SERVER_USERNAME}
 EOF
 
 # Execute deploy
